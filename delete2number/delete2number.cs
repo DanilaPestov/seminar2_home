c1eff3ec3bfad97180b7208dac8dd2997b133d6e
DanilaPestov <89147750075@mail.ru>
2022-04-21 00:46:22 +1000

    delete2number_01

--- delete2number/delete2number.cs
+++ delete2number/delete2number.cs
@@ -3,4 +3,4 @@ Console.WriteLine("наше число: " + number);
 int number1 = number % 10;
 number1 = number / 100;
 int number2 = number % 10;
-Console.WriteLine($"удалили вторую цифрц: {number1}{number2}");
+Console.WriteLine($"удалили вторую цифру: {number1}{number2}");
