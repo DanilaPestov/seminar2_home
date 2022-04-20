a772cdc6b88a5e5804939498fe82e6732f3a744b
DanilaPestov <89147750075@mail.ru>
2022-04-21 00:39:54 +1000

    delete2number

---
+++ delete2number/delete2number.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine("наше число: " + number);
+int number1 = number % 10;
+number1 = number / 100;
+int number2 = number % 10;
+Console.WriteLine($"удалили вторую цифрц: {number1}{number2}");
