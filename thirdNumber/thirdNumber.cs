c706eb1653763e247be64aee6671ec011935e6ee
DanilaPestov <89147750075@mail.ru>
2022-04-21 02:27:46 +1000

    thirdNumber

---
+++ thirdNumber/thirdNumber.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number <= 99)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+if (number > 99 & number <= 999)
+{
+    if (number % 10 == 0)
+    {
+        Console.WriteLine("третья цифра: 0");
+    }
+    else
+    {
+        Console.WriteLine("третья цифра: " + number % 10);
+    }
+}
+if (number > 999 & number <= 9999)
+{
+    if (number % 100 / 10 == 0)
+    {
+        Console.WriteLine("третья цифра: 0");
+    }
+    else
+    {
+        Console.WriteLine("третья цифра: " + number % 100 / 10);
+    }
+}
+if (number > 9999 & number <= 99999)
+{
+    if (number / 100 % 10 == 0)
+    {
+        Console.WriteLine("третья цифра: 0");
+    }
+    else
+    {
+        Console.WriteLine("третья цифра: " + number / 100 % 10);
+    }
+}
