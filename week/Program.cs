58584875e8c3d7dc84e169eaa38ef8f30005822e
DanilaPestov <89147750075@mail.ru>
2022-04-22 00:17:36 +1000

    week

---
+++ week/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("введите цифру, обозначающую день недели: ");
+int day = int.Parse(Console.ReadLine());
+if (day >= 1 & day <= 5)
+{
+    Console.WriteLine("рабочий день :)");
+}
+if (day >= 6 & day <= 7)
+{
+    Console.WriteLine("(: ВЫХОДНОЙ :)");
+}
+else Console.WriteLine("таких дней в неделе не существует");
